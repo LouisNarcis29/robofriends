08b5daa6633f68e7a37a36d7e6d88f969e2bd01d
Barbu Louis-Narcis <68915988+LouisNarcis29@users.noreply.github.com>
2022-10-07 16:19:17 +0300

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Louis Narcis",
+    username: "louisTheRobot",
+    email: "louistherobot@outlook.com",
+  },
+  {
+    id: 2,
+    name: "Marina",
+    username: "marinaPrettyRobot",
+    email: "marinarobotgirl@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Beatrice M.",
+    username: "beatriceMRobot",
+    email: "beatricethesmall@yahoo.com",
+  },
+  {
+    id: 4,
+    name: "Mihnea T.",
+    username: "mihneaRobot",
+    email: "mihneabigrobot@tor.ro",
+  },
+  {
+    id: 5,
+    name: "A. Andrei",
+    username: "andreiaa99",
+    email: "andreia.r99@rob.com",
+  },
+  {
+    id: 6,
+    name: "Mihnea Z.",
+    username: "mihneaBMW",
+    email: "mihneaTheRobot@beer.com",
+  },
+  {
+    id: 7,
+    name: "Radu Z.",
+    username: "raduTheKid",
+    email: "raduTheRobot@audi.es",
+  },
+  {
+    id: 8,
+    name: "George",
+    username: "toppaTheRobot",
+    email: "toppa@vali.ro",
+  },
+  {
+    id: 9,
+    name: "Don Ion",
+    username: "donIonRobot",
+    email: "donion@donion.com",
+  },
+  {
+    id: 10,
+    name: "M. Seb.",
+    username: "sebTheGoalkeeper",
+    email: "seb@apartot.com",
+  },
+];
